7ede379e8a9320f76d6475beaa95eead96ceb0e7
Aidan Rooney <aidanr1999@hotmail.com>
2020-03-31 23:10:22 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OregonTrail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var l = new Logic();
+            l.Start();
+        }
+    }
+}
